b1e39e41d4ad4bee5e62fe6fb373a38c8bb5dcbf
felipearce <felipearce@FELIPEARCEE9CE>
2021-10-15 12:04:49 -0400

    added calculator buttons

--- COP4226Assignment3/Form1.cs
+++ COP4226Assignment3/Form1.cs
@@ -55,5 +55,48 @@ namespace COP4226Assignment3
             openFileDialog1.Filter = "all supported (*.csv,*.txt)|*.csv;*.txt|csv files (*.csv)|*.csv|txt files (*.txt)|*.txt";
             openFileDialog1.ShowDialog();
         }
+
+        private void SendTextToTextBox(string str)
+        {
+            this.textBox1.Text += str;
+        }
+
+        private void ClearTextBox()
+        {
+            this.textBox1.Text = "";
+        }
+
+        private void CalculateResult(object sender, EventArgs e)
+        {
+            string text = this.textBox1.Text;
+            DataTable dt = new DataTable();
+            var v = new object();
+            try
+            {
+                Console.WriteLine("text: ", text);
+                v = dt.Compute(text, "");
+                Console.WriteLine("v: ", v);
+            }
+            catch (Exception x)
+            {
+                v = "NaN";
+                Console.WriteLine("exception e: ", x);
+            }
+
+
+            this.textBox1.Text = v.ToString();
+        }
+
+        private void AddButtonValueToTextBox(object sender, EventArgs e)
+        {
+            string buttonValue = (sender as Button).Text;
+            this.SendTextToTextBox(buttonValue);
+        }
+
+        private void ClearButtonHandler(object sender, EventArgs e)
+        {
+            this.ClearTextBox();
+        }
     }
+}
 }
